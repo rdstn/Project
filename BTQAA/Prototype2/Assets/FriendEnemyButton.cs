372df68baee21a50f31874f854990ed693119951
Radostin Nanov <rn619@york.ac.uk>
2015-11-20 20:13:28 +0000

    Friend/Enemy Buttons
    Friend/Enemy dynamic buttons. Still missing a data passing. Also
    discontinued efforts on dropdown, still no breakthrough on how to
    dynamically hide dropdown list elements.

---
+++ BTQAA/Prototype2/Assets/FriendEnemyButton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FriendEnemyButton : MonoBehaviour {
+
+    public UnityEngine.UI.Button self; //self
+    public UnityEngine.UI.Text tab; //tab button
+    public GameObject carrier; //The data carrier, used for transmission to the actual game session.
+
+	// Use this for initialization
+	void Start () {
+        self.GetComponentInChildren<Text>().text = tab.text;
+        self.image.color = Color.white;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        self.GetComponentInChildren<Text>().text = tab.text;
+    }
+
+    public void Clicked ()
+    {
+        if (self.image.color == Color.white)
+        {
+            self.image.color = Color.red;
+        }
+        else if (self.image.color == Color.red)
+        {
+            self.image.color = Color.green;
+        }
+        else
+        {
+            self.image.color = Color.white;
+        }
+    }
+}
--- BTQAA/Prototype2/Assets/TraitListScript.cs
+++ BTQAA/Prototype2/Assets/TraitListScript.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TraitListScript : MonoBehaviour {
+public class TraitListScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public UnityEngine.UI.Dropdown[] otherTraits;
 }
+    /*
+    public void SendOption(int input)
+    {
+        Debug.Log("An option has been picked! " + input);
+        if (input%2==0)
+        {
+            foreach (UnityEngine.UI.Dropdown x in otherTraits)
+            {
+                x.options = false;
+            }
+        }
+    }
+}
+*/
