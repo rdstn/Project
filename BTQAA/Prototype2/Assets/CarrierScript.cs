eeb49786c6120709b40439559d37d065aba60f59
Radostin Nanov <rn619@york.ac.uk>
2015-11-24 14:44:26 +0000

    Traits2Buttons Indev
    Moving the traits system from a dropdown to buttons to preserve my
    sanity.

--- BTQAA/Prototype2/Assets/CarrierScript.cs
+++ BTQAA/Prototype2/Assets/CarrierScript.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 [System.Serializable]
 public class CarrierScript : MonoBehaviour {
 
     public static CarrierScript current;
+    public int[] traitcounters;
     public string[] red1traits;
     public string[] red2traits;
     public string[] green1traits;
@@ -34,6 +36,7 @@ public class CarrierScript : MonoBehaviour {
     // Use this for initialization
     void Start () {
         int size = 10; //depends on the number of behaviour trees for which we set up priorities.
+        int[] traitcounters = new int[] { 0, 0, 0, 0, 0, 0 }; //Keep in store how many traits does a character have. We want a limit of 4.
         red1traits = new string[4];
         red2traits = new string[4];
         green1traits = new string[4];
@@ -100,37 +103,89 @@ public class CarrierScript : MonoBehaviour {
     }
 
     //Adding traits to actors, in RGB order, R1 to B2.
-    public void SetTrait (string trait, int character, int number)
+    public void SetTrait (string trait, int character)
     {
         if (character == 1)
         {
-            red1traits[number] = trait;
+            red1traits[0] = trait; //Since we sort empty strings to front, we can just push in the new trait to front.
+            Array.Sort(red1traits);            
         }
         else if (character == 2)
         {
-            red2traits[number] = trait;
+            red2traits[0] = trait;
+            Array.Sort(red2traits);
         }
         else if (character == 3)
         {
-            green1traits[number] = trait;
+            green1traits[0] = trait;
+            Array.Sort(green1traits);
         }
         else if (character == 4)
         {
-            green2traits[number] = trait;
+            green2traits[0] = trait;
+            Array.Sort(green2traits);
         }
         else if (character == 5)
         {
-            blue1traits[number] = trait;
+            blue1traits[0] = trait;
+            Array.Sort(blue1traits);
         }
         else if (character == 6)
         {
-            blue2traits[number] = trait;
+            blue2traits[0] = trait;
+            Array.Sort(blue2traits);
         }
         else
         {
             Debug.Log("Adding traits to a non-existent actor! " + character);
         }
+        traitcounters[character - 1]++; //Counter for how many traits a character has (up to 4, as per TraitList.cs).
+    }
 
+    public void RemoveTrait (string trait, int character)
+    {
+        if (character == 1)
+        {
+            int index = Array.IndexOf<string>(red1traits, trait); //Find the trait.
+            red1traits[index] = ""; //Remove it.
+            Array.Sort(red1traits); //Push empties to front.
+            
+        }
+        else if (character == 2)
+        {
+            int index = Array.IndexOf<string>(red2traits, trait);
+            red2traits[index] = "";
+            Array.Sort(red2traits);
+        }
+        else if (character == 3)
+        {
+            int index = Array.IndexOf<string>(green1traits, trait);
+            green1traits[index] = "";
+            Array.Sort(green1traits);
+        }
+        else if (character == 4)
+        {
+            int index = Array.IndexOf<string>(green2traits, trait);
+            green2traits[index] = "";
+            Array.Sort(green2traits);
+        }
+        else if (character == 5)
+        {
+            int index = Array.IndexOf<string>(blue1traits, trait);
+            blue1traits[index] = "";
+            Array.Sort(blue1traits);
+        }
+        else if (character == 6)
+        {
+            int index = Array.IndexOf<string>(blue2traits, trait);
+            blue2traits[index] = "";
+            Array.Sort(blue2traits);
+        }
+        else
+        {
+            Debug.Log("Removing traits from a non-existent actor! " + character);
+        }
+        traitcounters[character - 1]--;
     }
 
     //Adding relationships to actors, in RGB order, R1 to B2.
--- BTQAA/Prototype2/Assets/TraitList.cs
+++ BTQAA/Prototype2/Assets/TraitList.cs
@@ -2,83 +2,54 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class TraitList : MonoBehaviour {
-    public Dropdown itself; //References the object this script is attached to.
-    public Dropdown[] other_traits; //References the other trait selection dropdowns.
+public class TraitList : MonoBehaviour
+{
+    public Button itself; //References the object this script is attached to.
+    public Button opposite; //References the opposite trait.
+    public bool set; //References the state of the button - set or not.
     public GameObject carrier; //References the carrier object.
     public int character; //References the character whose traits we are editing.
-    //TODO: Rework to use the carrier, because otherwise it's problematic.
-	// Use this for initialization
-	void Start () {
-        //Traits work in pairs (except for none) - traits in a pair are mutually exclusive.
-        string[] new_options = { "None",
-            "Brave", "Craven",
-            "Outgoing", "Shy",
-            "Amorous", "Chaste",
-            "Gluttonous", "Temperate",
-            "Greedy", "Charitable",
-            "Lazy", "Energetic",
-            "Wrathful", "Patient",
-            "Envious", "Kind",
-            "Proud", "Humble",
-            "Paranoid", "Trusting",
-            "Cruel", "Honorable" };
-        for (int i=0; i<=new_options.Length; i++)
-        {
-            itself.options[i].text = new_options[i];
-        }
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public string trait; //The trait manipulated by this object.
 
-    public void Select (int input)
+    // Use this for initialization
+    void Start()
     {
-        //This is needed for mutual exclusiviy of traits.
-        for (int i = 0; i < other_traits.Length; i++)
-        {
-            other_traits[i].GetComponent<TraitList>().Change(itself.options[input].text);
-        }
+        itself.GetComponentInChildren<Text>().text = trait;
+        itself.image.color = Color.white;
     }
 
-
-    //Call when we make a change. The input will be the index of the option selected.
-    public void Change (string input)
+    // Update is called once per frame
+    void Update()
     {
-        ArrayList options = new ArrayList { "None",
-            "Brave", "Craven",
-            "Outgoing", "Shy",
-            "Amorous", "Chaste",
-            "Gluttonous", "Temperate",
-            "Greedy", "Charitable",
-            "Lazy", "Energetic",
-            "Wrathful", "Patient",
-            "Envious", "Kind",
-            "Proud", "Humble",
-            "Paranoid", "Trusting",
-            "Cruel", "Honorable" };
-        if (options.Contains(input) && input!="None")
+        if (carrier.GetComponent<CarrierScript>().traitcounters[character-1]==3)
         {
-            int selection=options.IndexOf(input);
-            options.Remove(input);
-            if (selection % 2 != 0)
+            if (set== false)
             {
-                options.RemoveAt(selection + 1);
-            }
-            else
-            {
-                options.RemoveAt(selection - 1);
+                itself.image.color = Color.red;
+                itself.interactable = false;
             }
         }
-        string [] new_options = (string[]) options.ToArray(typeof(string));
-        for (int i = 0; i <= itself.options.Count/2; i++)
-        {
-            itself.options[i].text = new_options[i];
-        }
+    }
 
+    public void Press ()
+    {
+        if (set == false)
+        {
+            set = true;
+            carrier.GetComponent<CarrierScript>().SetTrait(trait, character);
+            opposite.interactable = false;
+            opposite.image.color = Color.red;
+            itself.image.color = Color.green;
+        }
+        else if (set == true)
+        {
+            set = false;
+            carrier.GetComponent<CarrierScript>().RemoveTrait(trait, character);
+            opposite.interactable = true;
+            opposite.image.color = Color.white;
+            itself.image.color = Color.white;
+        }
     }
-    }
+
+}
 
